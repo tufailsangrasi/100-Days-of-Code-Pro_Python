2ef39a8a0d4c908eba32f092775c365e8c6f266a
tufailsangrasi <tufailahmedsangrasi11@gmail.com>
2025-10-02 00:02:19 +0500

    compile and runtime polymorphism

--- day_19.py
+++ day_19.py
@@ -1,6 +1,6 @@
 # Polymorphism 
 # 1.Compile time polymorphism -> Overloading (same name , different parameters)
-# 2.Runtime Polymorphism -> Overriden
+# 2.Runtime Polymorphism -> Overriden 
 class university:
 
   def info(self , name):
@@ -14,6 +14,7 @@ class university:
     print(self.name)
     print(self.location)
 
+# Main methods
 muet = university()
 muet.info("Mehran1")
 muet.info("Mehran" , "Jamshoro")
